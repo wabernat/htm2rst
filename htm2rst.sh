c093b99e738d6ef3480e080e235f05245bdead63
William Abernathy <william.abernathy@scality.com>
2018-09-14 16:15:39 -0700

    fixed bug in admonitions, bad img refs

--- htm2rst.sh
+++ htm2rst.sh
@@ -7,32 +7,28 @@
 # and change all of the Box (admonition) p-tags to tags that RST can parse.
 # After this, the file is passed to htm2rst.sh for conversion to RST.
 
-
-
 rsync -a ./Content ./Interim_source
 
-cd ./Interim_source
-
 printf "Removing ChapNum.\n"
-find ./ -name *.htm -exec sed -i 's#<p\ class=\"ChapterNumber\">*<\/p># #g' {} +
+find ./Interim_source -name *.htm -exec sed -i 's|<p\ class=\"ChapterNumber\">|<\/p>|g' {} +
 
 printf "Looking for various Note tags.\n"
-find ./ -name *.htm -exec sed -i 's#<p\ class=\"Note\">#<p>..\ note::\ #g' {} +
-find ./ -name *.htm -exec sed -i 's#<p\ class=\"NoteUnder\">#<p>..\ note::\ #g' {} +
-find ./ -name *.htm -exec sed -i 's#<p\ class=\"BoxNote\">#<p>..\ note::\ #g' {} +
+find ./Interim_source -name *.htm -exec sed -i 's|<p\ class=\"Note\">|<p>..\ note::\n&|g' {} +
+find ./Interim_source -name *.htm -exec sed -i 's|<p\ class=\"NoteUnder\">|<p>..\ note::\n&|g' {} +
+find ./Interim_source -name *.htm -exec sed -i 's|<p\ class=\"BoxNote\">|<p>..\ note::\n&|g' {} +
 
 printf "Looking for Tips.\n"
-find ./ -name *.htm -exec sed -i 's#<p\ class=\"BoxTip\">#<p>..\ tip::\ #g' {} +
+find ./Interim_source -name *.htm -exec sed -i "s|<p\ class=\"BoxTip\">|<p>..\ tip::\n&|g" {} +
 
 printf "Looking for BoxImportant.\n"
-find ./ -name *.htm -exec sed -i 's#<p\ class=\"BoxIMPORTANT\">#<p>..\ important::\ #g' {} +
-find ./ -name *.htm -exec sed -i 's#<p\ class=\"Alert\">#<p>..\ important::\ #g' {} +
-printf "Looking for Warnings.\n"
-find ./ -name *.htm -exec sed -i 's#<p\ class=\"BoxWARNING\">#<p>..\ warning::\ #g' {} +
-find ./ -name *.htm -exec sed -i 's#<p\ class=\"EmphasisCRITICAL\">#<p>..\ warning::\ #g' {} +
+ find ./Interim_source -name *.htm -exec sed -i "s|<p\ class=\"BoxIMPORTANT\">|<p>..\ important::\n&|g" {} +
+ find ./Interim_source -name *.htm -exec sed -i "s|<p\ class=\"Alert\">|<p>..\ important::\n&|g" {} +
 
-#Housekeeping: if there is no RST directory, build one:
-cd ..
+printf "Looking for Warnings.\n"
+ find ./Interim_source -name *.htm -exec sed -i "s|<p\ class=\"BoxWARNING\">|<p>..\ warning::\n&|g" {} +
+ find ./Interim_source -name *.htm -exec sed -i "s|<p\ class=\"EmphasisCRITICAL\">|<p>..\ warning::\n&|g" {} +
+
+# Housekeeping: if there is no RST directory, build one:
 
     if [ -d RST ] ;
     then
@@ -50,7 +46,7 @@ cd ..
 
 printf "Converting...\n"
 
-find ./Interim_source/Content -name '*.htm' -print > FileList.txt;
+find ./Interim_source/Content -name '*.htm' | sed 's|\.\/Interim_source\/||g' > FileList.txt
 
 while read line ;
 do
@@ -58,22 +54,30 @@ do
     # (throw out ".htm"). What's left is the filename.
     filename=${line##*/}
     filename=${filename%.*}
+    newfilename=$(echo $filename | sed 's|\ |_|g')
 
+ #   printf "newfilename = $newfilename \n"
+    
     # Backwards pattern match $line to the last slash (the first one it runs
     # into). What's left is the dirname. It's probably better to do all this
     # with dirname and basename commands, but I know this works.
     dirname=${line%/*}
+    newdirname=$(echo $dirname | sed 's|\ |_|g')
 
-    # If there is no directory name in the RST output that matches the current
-    # dirname, then build one.
-    if [ ! -d "RST/$dirname" ] ;
+#    printf "newdirname = $newdirname\n"
+    
+    # If there is no directory name in the RST output that matches $newdirname,
+    # then build one.
+
+    if [ ! -d "RST/$newdirname" ] ;
     then
-	mkdir -p RST/"$dirname"
+	mkdir -p RST/"$newdirname"
 	fi ;
 
     # pandoc-convert $filename from htm to rst.
-    # Output the results to ./RST/$dirname/$filename.rst
-   pandoc -f html -t rst "$dirname/$filename.htm" > "RST/$dirname/$filename.rst"
+    # Output the results to ./RST/$newdirname/$newfilename.rst
+
+    pandoc -f html -t rst "./Interim_source/$dirname/$filename.htm" > "RST/$newdirname/$newfilename.rst"
 
 done < FileList.txt
 
@@ -85,13 +89,13 @@ printf "Done with RST conversion.\n"
 
 # Now, image files. Grep for anything in the RST that asks for a graphic file.
 # Store the results in img_list.txt
+
 printf "Searching RST files for image references.\n"
 
-egrep -ri "*.png" ./RST/Content/* --exclude-dir=./RST/Content/Resources > img_list.txt
-egrep -ri "*.svg" ./RST/Content/* --exclude-dir=./RST/Content/Resources >> img_list.txt
-egrep -ri "*.jpg" ./RST/Content/* --exclude-dir=./RST/Content/Resources >> img_list.txt
-egrep -ri "*.gif" ./RST/Content/* --exclude-dir=./RST/Content/Resources >> img_list.txt
-
+egrep -ri "*.png" ./RST/Content/* --exclude-dir=./RST/Content/Resources | sed 's|%20|\_|g' > img_list.txt
+egrep -ri "*.svg" ./RST/Content/* --exclude-dir=./RST/Content/Resources | sed 's|%20|\_|g' >> img_list.txt
+egrep -ri "*.jpg" ./RST/Content/* --exclude-dir=./RST/Content/Resources | sed 's|%20|\_|g' >> img_list.txt
+egrep -ri "*.gif" ./RST/Content/* --exclude-dir=./RST/Content/Resources | sed 's|%20|\_|g' >> img_list.txt
 
 while read line;
 do
@@ -113,10 +117,10 @@ fi
 
  # From pathlist.txt, read the path and the file name, and write it to
  # the RST Images directory.
- printf "Copying the images.\n"
+
+printf "Copying the images.\n"
 while read img_path;
 do
-
     if [ ! -d "./RST/Content/Resources/Images/$(dirname $img_path)" ] ;
     then
     mkdir -p  "./RST/Content/Resources/Images/$(dirname $img_path)"
@@ -128,5 +132,6 @@ printf "Tidying up.\n"
 
 rm pathlist.txt
 rm img_list.txt
+rm -rf Interim_source/
 
 printf "Done.\n"
